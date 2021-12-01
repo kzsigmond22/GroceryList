5d633b9237b7cf9a993348a74b3505eb4f581e69
zkathi <k.zsigmond@outlook.com>
2021-12-01 22:52:08 +0100

    Create android project

---
+++ app/src/main/java/com/zkathi/grocerylist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zkathi.grocerylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zkathi.grocerylist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
