d84bdb4b8bab4cb9d71b25824496e1b2594964c3
zkathi <k.zsigmond@outlook.com>
2021-12-07 17:46:12 +0100

    Add action bar

--- app/src/main/java/com/zkathi/grocerylist/ui/MainActivity.kt
+++ app/src/main/java/com/zkathi/grocerylist/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.zkathi.grocerylist.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.zkathi.grocerylist.R
 import com.zkathi.grocerylist.ui.grocerylist.GroceryListFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,10 +16,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, GroceryListFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_add_grocery -> {
+            // Go to new fragment
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
         }
     }
 }
