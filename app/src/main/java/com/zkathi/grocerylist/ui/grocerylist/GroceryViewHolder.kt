9a5886a63384e982d08a5ca224f5f552eaa87545
zkathi <k.zsigmond@outlook.com>
2021-12-30 21:35:28 +0100

    add UI functionality to list item -/+ icons

--- app/src/main/java/com/zkathi/grocerylist/ui/grocerylist/GroceryViewHolder.kt
+++ app/src/main/java/com/zkathi/grocerylist/ui/grocerylist/GroceryViewHolder.kt
@@ -7,8 +7,23 @@ import com.zkathi.grocerylist.databinding.ItemGroceryBinding
 class GroceryViewHolder(private val binding: ItemGroceryBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    private lateinit var grocery: Grocery
+
     fun bind(grocery: Grocery) {
+        this.grocery = grocery
+        binding.groceryVH = this
         binding.grocery = grocery
     }
 
+    fun increaseQuantity() {
+        grocery.quantity++
+        binding.grocery = grocery
+    }
+
+    fun decreaseQuantity() {
+        if (grocery.quantity > 0) {
+            grocery.quantity--
+            binding.grocery = grocery
+        }
+    }
 }
